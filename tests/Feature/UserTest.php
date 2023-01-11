30f0255ca03f539b1d180165bb4d9ef58edec68c
Luciano Bosco <lucianobosco@gmail.com>
2023-01-11 18:52:08 +0100

    Created User migration feature test (TDD)

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_migrate_posts()
+    {
+        $this->post('/api/users/migrate')
+            ->assertOk()
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->whereAllType([
+                    'data.0.id' => 'integer',
+                    'data.0.name' => 'string',
+                    'data.0.email' => 'string',
+                    'data.0.city' => 'integer',
+                    'message' => 'string'
+                ])
+            );
+    }
+}
