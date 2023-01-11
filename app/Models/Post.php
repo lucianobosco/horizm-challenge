88fbf9e237b549a8305c78e2d035825012a3aaee
Luciano Bosco <lucianobosco@gmail.com>
2023-01-11 17:47:57 +0100

    Created Post Model

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'user_id',
+        'title',
+        'body',
+        'rating'
+    ];
+
+    /**
+     * Get the user associated with the post.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
